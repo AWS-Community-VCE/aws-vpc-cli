9b108c2118b761a4e93cb9cd44a291e7cafde289
Utej <91786744+utej8553@users.noreply.github.com>
2026-08-06 16:39:26 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ REGION = "us-east-1"
 
 echo "creating vpc: "
 
-VPC_ID = $(aws ec2  create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId' --output text --region $REGION)
+VPC_ID = $(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId' --output text --region $REGION)
 
 echo "VPC: $VPC_ID"
 
