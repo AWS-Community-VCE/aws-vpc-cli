50c829d87ea2c549f2203f2986176acc0d3bcc32
Utej <91786744+utej8553@users.noreply.github.com>
2026-08-06 16:12:26 +0530

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REGION = us-east-1
+VPC_NAME = DEMO_VPC
+CIDR = 10.0.0.0/16 # 65,536 ips
+PUBLIC1 = 10.0.1.0/24 # 256 ips 
+PUBLIC1 = 10.0.2.0/24 # 256 ips 
+PRIVATE1 = 10.0.11.0/24 # 256 ips 
+PRIVATE2 = 10.0.12.0/24 # 256 ips 
